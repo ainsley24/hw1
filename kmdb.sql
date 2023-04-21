b9e4a7338e79a0b5a19ec7e5020d1d8386fe70b5
Ainsley Campbell <ainsley.campbell@kellogg.northwestern.edu>
2023-04-21 02:53:41 +0000

    now it runs

--- kmdb.sql
+++ kmdb.sql
@@ -110,8 +110,9 @@ DROP TABLE IF EXISTS actors;
 DROP TABLE IF EXISTS roles; 
 
 -- Create new tables, according to your domain model
+
 CREATE TABLE movies (
-    id INTEGER PRIMARY KEY AUTOINCREMENT 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     year NUMBER,
     rating TEXT,
@@ -120,18 +121,18 @@ CREATE TABLE movies (
 );
 
 CREATE TABLE studios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     studio_name TEXT
 );
 
 CREATE TABLE actors (
-    id INTEGER PRIMARY KEY AUTOINCREMENT 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     first_name TEXT,
-    last_name TEXT,
+    last_name TEXT
 );
 
 CREATE TABLE roles (
-    id INTEGER PRIMARY KEY AUTOINCREMENT 
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     actor_id INTEGER,
     movie_id INTEGER,
     character TEXT
@@ -140,7 +141,13 @@ CREATE TABLE roles (
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+INSERT INTO movies (title, year, rating, studio_name)
+VALUES 
+('Batman Begins', 2005, 'PG-13', 'Warner Bros.'),
+('The Dark Knight', 2008, 'PG-13', 'Warner Bros.'),
+('The Dark Knight Rises', 2012, 'PG-13', 'Warner Bros.');
+
+
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -148,7 +155,7 @@ CREATE TABLE roles (
 .print ""
 
 -- The SQL statement for the movies output
--- TODO!
+SELECT * FROM movies; 
 
 -- Prints a header for the cast output
 .print ""
